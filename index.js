11996ecf0b52fc5cabd666e94de5c242a20a4702
psalmpy <psalmpally@gmail.com>
2023-02-14 22:49:55 +0100

    modified index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-let inputBtn = document.getElementById("input-btn")
+const inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function(){
     console.log("The button is clicked from event listener, thanks")
 })
+
+let myLead = []
+const inputEl = document.getElementById("input-el")
